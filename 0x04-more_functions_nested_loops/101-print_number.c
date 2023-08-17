2d670fb4248b807ad04216fc89cf6f3454f7b80e
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-17 09:31:07 -0700

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ * print_number - printsinteger
+ * @n: integer tobeprinted
  */
 void print_number(int n)
 {
